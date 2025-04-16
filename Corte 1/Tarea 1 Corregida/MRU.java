4319c854def45d8cfc47fe2714937cbf1b2b2330
EdgarMolina65 <edgarfrenmol@gmail.com>
2025-04-16 00:20:35 -0500

    Add files via upload

---
+++ Corte 1/Tarea 1 Corregida/MRU.java
@@ -0,0 +1,12 @@
+public class MRU extends Movimiento {
+    @Override
+    public void calcular(Consola consola) {
+        double v = consola.leerDouble("Ingresa la velocidad (m/s): ");
+        double t = consola.leerDoublePositivo("Ingresa el tiempo (s): ");
+        double d = v * t;
+        
+        System.out.println("Distancia recorrida: " + d +" metros");
+        System.out.println("Velocidad: " + v +" m/s");
+
+    }
+}
