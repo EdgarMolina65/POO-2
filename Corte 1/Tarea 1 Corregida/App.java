42427e0e9584ea06bf813393ad138df353bd7484
EdgarMolina65 <edgarfrenmol@gmail.com>
2025-04-16 00:28:58 -0500

    Delete Corte 1/Tarea 1 Corregida directory

--- Corte 1/Tarea 1 Corregida/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        Vista vista = new Vista();
-        vista.mostrarMenu();  // Llamamos al menú de la vista para que se inicie el programa
-    }
-}
-
