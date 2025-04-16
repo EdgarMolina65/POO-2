a6424500ba65e0d417698a59c2180dfa89a8bd87
EdgarMolina65 <edgarfrenmol@gmail.com>
2025-04-16 00:24:20 -0500

    Delete Corte 1/Tarea 1 directory

--- Corte 1/Tarea 1/Movimiento.java
+++
@@ -1,4 +0,0 @@
-public abstract class Movimiento {
-    // Método abstracto para ser implementado en las clases hijas
-    public abstract void calcular(Consola consola);
-}
--- Corte 1/Tarea 1/ReadMe.txt
+++
@@ -1,1 +0,0 @@
-Esta es la Calculadora de Movimientos 
